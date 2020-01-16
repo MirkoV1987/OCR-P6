1518ca068c8506e36f6dcf4c4f0a0f8c7fc2a865
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-01-16 11:38:10 +0100

    Upload config folder

---
+++ config/bundles.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    App\OCRP6\TestBundle\Ocrp6TestBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+];
