a058b193bbd6de0455daa6287fff5dfd6d064870
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-02-08 19:07:18 +0100

    Upload Migrations

---
+++ src/Migrations/Version20200208173456.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200208173456 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE image ADD trick_id INT DEFAULT NULL, DROP trick, CHANGE url url VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE image ADD CONSTRAINT FK_C53D045FB281BE2E FOREIGN KEY (trick_id) REFERENCES trick (id) ON DELETE CASCADE');
+        $this->addSql('CREATE INDEX IDX_C53D045FB281BE2E ON image (trick_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE image DROP FOREIGN KEY FK_C53D045FB281BE2E');
+        $this->addSql('DROP INDEX IDX_C53D045FB281BE2E ON image');
+        $this->addSql('ALTER TABLE image ADD trick DATETIME NOT NULL, DROP trick_id, CHANGE url url INT NOT NULL');
+    }
+}
