9bd0b02b2b5c223d3ddef0203ce6f5697ab847ca
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-02-15 17:32:59 +0100

    Upload UploaderHelper Service

---
+++ src/Service/UploaderHelper.php
@@ -0,0 +1,29 @@
+<?php
+
+// src/Service/UploaderHelper
+
+namespace App\Service;
+
+use Gedmo\Sluggable\Util\Urlizer;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class UploaderHelper
+{
+    private $uploadsPath;
+
+    public function __construct(string $uploadsPath)
+    {
+        $this->uploadsPath = $uploadsPath;
+    }
+    public function uploadTrickImage(UploadedFile $uploadedFile): string
+    {
+        $destination = $this->uploadsPath.'/uploads';
+        $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+        $newFilename = Urlizer::urlize($originalFilename).'-'.uniqid().'.'.$uploadedFile->guessExtension();
+        $uploadedFile->move(
+            $destination,
+            $newFilename
+        );
+        return $newFilename;
+    }
+}
