94ab1d542b792bd08dbc73ec7266718c325d0fc7
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-03-21 23:03:03 +0100

    Update VideoType

--- src/Form/VideoType.php
+++ src/Form/VideoType.php
@@ -2,10 +2,11 @@
 
 namespace App\Form;
 
-use App\Entity\Media;
+use App\Entity\Video;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -34,7 +35,7 @@ class VideoType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Media::class,
+            'data_class' => Video::class,
         ]);
     }
 }
