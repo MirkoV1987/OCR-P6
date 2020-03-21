ebc29a34f1d668e615196475acf970fa78e0c5d8
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-03-21 23:06:53 +0100

    Add VideoUploader Service

--- src/Service/VideoUploader.php
+++ src/Service/VideoUploader.php
@@ -1,170 +1,35 @@
 <?php
 
+// src/Service/UploaderHelper
+
 namespace App\Service;
 
-use Symfony\Component\Config\Definition\Exception\Exception;
+use App\Entity\Trick;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\ErrorHandler\ErrorHandler;
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
 
-/**
- * This class transform simple video url from dailymotion or youtube to an embed video link usable with iframe HTML tag.
- * Class VideoUploader
- * @package App\Services
- */
 class VideoUploader
 {
+    private $params;
+    private $em;
 
-    /**
-     * @var string
-     */
-    private $youtubeREGEX;
-
-    /**
-     * @var string
-     */
-    private $dailymotionREGEX;
-
-    /**
-     * @var string
-     */
-    private $youtube;
-
-    /**
-     * @var string
-     */
-    private $dailymotion;
-
-    /**
-     * @var string
-     */
-    private $url;
-
-    /**
-     * @return string
-     */
-    /**
-     * @var
-     */
-    private $embedUrl;
-
-    /**
-     * @return string
-     */
-    public function getEmbedUrl(): string
+    public function __construct(ContainerBagInterface $params, EntityManagerInterface $em)
     {
-        return $this->embedUrl;
+        $this->params = $params;
+        $this->em = $em;
     }
 
-
-
-    public function getYoutubeREGEX(): string
+    public function uploadTrickVideo(array $videos, Trick $trick)
     {
-        return $this->youtubeREGEX;
+        $videos = $trick->getVideos();
+        foreach ($videos as $video) {
+            $video->setTrick($trick);
+            $video->setUrl($video->getUrl());
+            $this->em->persist($video);
+            $this->em->flush();
+        }
+
+        return $videos;
     }
-
-    /**
-     * @param string $youtubeREGEX
-     */
-    public function setYoutubeREGEX(string $youtubeREGEX): void
-    {
-        $this->youtubeREGEX = $youtubeREGEX;
-    }
-
-    /**
-     * @return string
-     */
-    public function getDailymotionREGEX(): string
-    {
-        return $this->dailymotionREGEX;
-    }
-
-    /**
-     * @param string $dailymotionREGEX
-     */
-    public function setDailymotionREGEX(string $dailymotionREGEX): void
-    {
-        $this->dailymotionREGEX = $dailymotionREGEX;
-    }
-
-    /**
-     * @return string
-     */
-    public function getYoutube(): string
-    {
-        return $this->youtube;
-    }
-
-    /**
-     * @param string $youtube
-     */
-    public function setYoutube(string $youtube): void
-    {
-        $this->youtube = $youtube;
-    }
-
-    /**
-     * @return string
-     */
-    public function getDailymotion(): string
-    {
-        return $this->dailymotion;
-    }
-
-    /**
-     * @param string $dailymotion
-     */
-    public function setDailymotion(string $dailymotion): void
-    {
-        $this->dailymotion = $dailymotion;
-    }
-
-    /**
-     * @return string
-     */
-    public function getUrl()
-    {
-        return $this->url;
-    }
-
-    /**
-     * @param string
-     */
-    public function setUrl($url)
-    {
-        $this->url = $url;
-    }
-
-
-    /**
-     * EmbedURL constructor.
-     * @param string $url
-     */
-    // public function __construct()
-    // {
-    //     $url = $this->url; 
-    //     $this->youtubeREGEX = '/youtube/';
-    //     $this->dailymotionREGEX = '/dailymotion/';
-    //     return $this->embedUrl = $this->embed($url);
-    // }
-
-    /**
-     * @param $url
-     * @return bool|mixed
-     */
-    // public function embed($url)
-    // {
-    //     $this->youtube = preg_match($this->youtubeREGEX, $url);
-    //     $this->dailymotion = preg_match($this->dailymotionREGEX, $url);
-
-    //     if ($url && $this->youtube) {
-    //         str_replace("watch?v=", "embed/", $url);
-    //         return $this->embedUrl;
-    //     }
-    //     if ($url && $this->dailymotion) {
-    //         str_replace("video", "embed/video", $url);
-    //         return $this->embedUrl;
-    //     } else {
-    //         throw new Exception(
-    //             'URL incorrecte'
-    //         );
-    //     }    
-    // }
 }
