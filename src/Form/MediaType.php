de49e0d35423ca077c3483c0422861f54f834110
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-03-23 22:35:19 +0100

    Update MediaType

--- src/Form/MediaType.php
+++ src/Form/MediaType.php
@@ -8,6 +8,7 @@ use App\Entity\Media;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,14 +17,34 @@ class MediaType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', FileType::class, [
-                    'label' => false,
-                    'row_attr' => ['class' => 'add-image'],
-                    'attr' => [
-                        'placeholder' => 'Charger une image',
-                    ],
-                    'required'=> true 
+        ->add('name', FileType::class, [
+            'label' => false,
+            //'mapped'=> true,
+            'row_attr' => ['class' => 'add-image'],
+            'attr' => [
+                'placeholder' => 'Charger une image',
+            ],
+            'required'=> true,
+            //'multiple'=> true,
+            // 'constraints' => [
+            //     new Media([
+            //         'maxSize' => '4M',
+            //         'mimeTypes' => [
+            //             'application/jpeg',
+            //             'application/jpg',
+            //             'application/png',
+            //         ],
+            //         'mimeTypesMessage' => 'Charger un format valide',
+            //     ])
+            // ],
             ])
+            // ->add('name', TextType::class, [
+            //         'label' => false,
+            //         'required'=> false,
+            //         'attr' => [
+            //             'placeholder' => 'Titre de l\'image',
+            //         ],
+            // ])
             ->add('caption', TextType::class, [
                 'label' => false,
                 'attr' => [
@@ -39,7 +60,7 @@ class MediaType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Media::class,
+            'data_class' => Media::class
         ]);
     }
 }
