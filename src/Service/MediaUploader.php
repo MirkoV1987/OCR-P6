8a605ae54c97a74af8b75f8305c05dbcb0bcfd14
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-03-28 22:57:24 +0100

    Update MediaUploader Service

--- src/Service/MediaUploader.php
+++ src/Service/MediaUploader.php
@@ -4,57 +4,23 @@
 
 namespace App\Service;
 
-use Gedmo\Sluggable\Util\Urlizer;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\ErrorHandler\ErrorHandler;
 use App\Entity\Media;
-use App\Entity\Trick;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
 
 class MediaUploader
 {
-    private $params;
-    private $em;
-    private $uploadsPath;
-
-    public function __construct(string $uploadsPath, ContainerBagInterface $params, EntityManagerInterface $em)
+    public function manageMedia(Media $media): Media
     {
-        $this->params = $params;
-        $this->em = $em;
-        $this->uploadsPath = $uploadsPath;
-    }
+        /** @var UploadedFile $file */
+        $file = $media->getFile();
 
-    public function uploadTrickMedia(UploadedFile $uploadedFile)
-    {
-        $destination = $this->uploadsPath.'/public/uploads/images/tricks';
-        $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
-        $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
-        $fileName = $safeFilename.'-'.uniqid().'.'.$uploadedFile->guessExtension();
+        $fileName = md5(uniqid()) . '.' . $file->guessExtension();
+       
+        $path = 'uploads/images/tricks';
+        $file->move($path, $fileName);
 
-        try {
-            $uploadedFile->move($destination, $fileName);
-        } catch (FileException $e) {
-            throw new Exception("Erreur de chargement du fichier");
-        }
+        $media->setName($fileName);
 
-        return $fileName;
-    }
-
-    public function manageMedia(array $medias, Trick $trick)
-    {
-        $medias = $trick->getMedias();
-        foreach ($medias as $media) {
-            $media->setTrick($trick);
-            $media->setName($media->getName());
-            //$media->setImageFile($media->getImageFile());
-            $media->setCaption($media->getCaption());
-            $now = new \DateTime('+ 1 hour');
-            $media->getDateAdd($now);
-            $this->em->persist($media);
-            $this->em->flush();
-        }
-
-        return $medias;
+        return $media;
     }
 }
