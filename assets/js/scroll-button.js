b557393f73122d65facd788f9d0740d062850740
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-04-09 17:54:22 +0200

    Upload new assets files

---
+++ assets/js/scroll-button.js
@@ -0,0 +1,14 @@
+
+/*****Scroll button*******/
+
+$(function () {
+  $('#scrollToBottom').bind("click", function () {
+      $('html, body').animate({ scrollTop: $(document).height() }, 1200);
+      return false;
+  });
+  $('#scrollToTop').bind("click", function () {
+      $('html, body').animate({ scrollTop: 0 }, 1200);
+      return false;
+  });
+});
+
