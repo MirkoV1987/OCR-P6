f5a11dc70d0739e63c9788e867eec6466dd5fcb9
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-04-17 21:11:03 +0200

    Upload Migrations

---
+++ src/Migrations/Version20200417185814.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200417185814 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE trick DROP FOREIGN KEY FK_D8F0A91EF675F31B');
+        $this->addSql('DROP INDEX IDX_D8F0A91EF675F31B ON trick');
+        $this->addSql('ALTER TABLE trick CHANGE author_id user_id INT NOT NULL');
+        $this->addSql('ALTER TABLE trick ADD CONSTRAINT FK_D8F0A91EA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_D8F0A91EA76ED395 ON trick (user_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE trick DROP FOREIGN KEY FK_D8F0A91EA76ED395');
+        $this->addSql('DROP INDEX IDX_D8F0A91EA76ED395 ON trick');
+        $this->addSql('ALTER TABLE trick CHANGE user_id author_id INT NOT NULL');
+        $this->addSql('ALTER TABLE trick ADD CONSTRAINT FK_D8F0A91EF675F31B FOREIGN KEY (author_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_D8F0A91EF675F31B ON trick (author_id)');
+    }
+}
