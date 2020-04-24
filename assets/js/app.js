01650fd19f1c190ba1cd8b107ab6a249d341228d
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-04-24 21:51:25 +0200

    Delete assets

--- assets/js/app.js
+++
@@ -1,8 +0,0 @@
-// assets/js/app.js
-
-require('../css/app.css');
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
-
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-//import $ from 'jquery';
