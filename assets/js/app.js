b80052b445079b9c8b4d2b137c6ae1c886f852dc
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-04-25 21:45:46 +0200

    Update import file path

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,8 +6,8 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../assets/css/app.css';
-import '../assets/css/style.css'
+import '../css/app.css';
+import '../css/style.css'
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
