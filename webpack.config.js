658fd8ef2e874ffd1a296e7b2766ddb2237b0349
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-04-25 21:46:19 +0200

    Update Encore Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
-    .setPublicPath('../public/build')
+    .setPublicPath('http://localhost/OCR-P6/public/build')
     // only needed for CDN's or sub-directory deploy
     .setManifestKeyPrefix('public/build/')
 
