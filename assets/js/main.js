3937b94e9d9083bc1faaf477777f0d5a4f902650
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-05-02 15:24:10 +0200

    Upload main.js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,14 @@ $ (function () {
     return false;
   });
 });
+
+/**----------------------------------------------------
+--------------Display uploaded Filename----------------
+----------------------------------------------------**/
+
+$('.custom-file-input').on('change', function(event) {
+  var inputFile = event.currentTarget;
+  $(inputFile).parent()
+      .find('.custom-file-label')
+      .html(inputFile.files[0].name);
+});
