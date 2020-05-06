610d1ff1ad32cc5716eae86cc607e54f0026826b
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-05-06 17:05:01 +0200

    Upload ResetPassword Entity

---
+++ src/Form/Model/ResetPassword.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Form\Modal;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class ResetPassword
+{
+    /**
+     * @Assert\NotBlank(
+     *      message = "Ce champ est requis !"
+     * )
+     * @Assert\Length(
+     *      min = 8,
+     *      max = 254,
+     *      minMessage = "Votre mot de passe doit contenir au moins 8 caractères.",
+     *      maxMessage = "Votre mot de passe ne peut pas contenir plus que {{ limit }} caractères !"
+     * )
+     * @Assert\Regex(
+     *     pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W)^",
+     *     match = true,
+     *     message = "Le mot de passe doit contenir au moins une minuscule, une majuscule, un chiffre et un caractère spécial !"
+     * )
+     */
+    private $newPassword;
+
+    /**
+     * @Assert\EqualTo(
+     *      propertyPath = "newPassword",
+     *      message = "Le mot de passe n'est pas identique."
+     * )
+     */
+    private $confirmPassword;
+    
+
+    public function getNewPassword(): ?string
+    {
+        return $this->newPassword;
+    }
+
+    public function setNewPassword(string $newPassword): self
+    {
+        $this->newPassword = $newPassword;
+
+        return $this;
+    }
+
+    public function getConfirmPassword(): ?string
+    {
+        return $this->confirmPassword;
+    }
+
+    public function setConfirmPassword(string $confirmPassword): self
+    {
+        $this->confirmPassword = $confirmPassword;
+
+        return $this;
+    }
+}
